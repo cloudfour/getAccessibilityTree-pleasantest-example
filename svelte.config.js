e55d09513c1e14cab7d31d9ed8d5b53353432303
Caleb Eby <caleb.eby01@gmail.com>
2022-02-01 11:12:18 -0800

    Add a fallback so the 404 pages don't cause the build to fail

--- svelte.config.js
+++ svelte.config.js
@@ -3,23 +3,23 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess({
-		postcss: true
-	}),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess({
+    postcss: true,
+  }),
 
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false
-		}),
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'src/app.html',
+      precompress: false,
+    }),
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+  },
 };
 
 export default config;
